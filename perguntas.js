23a713819bbf379a1406897e73c7b7224ca7b1a2
oliviarodrigos <isabela.coutinho.brito@escola.pr.gov.br>
2025-05-30 01:03:28 +0000

    aula 6 criando js

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Fósforo?',
+    'O o número atômico do Fósforo é 15.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Carbono?',
+    'O número atômico do Carbono é 6.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Boro',
+    'O número atômico do Boro é 5.'
+)
