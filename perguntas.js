8f8a184b855a72dd779719cbbdc6bdf35b11a21a
oliviarodrigos <isabela.coutinho.brito@escola.pr.gov.br>
2025-05-30 02:20:08 +0000

    aula 8 elementos js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Química',
-    'Qual é o número atômico do Fósforo?',
-    'O o número atômico do Fósforo é 15.'
+    'Qual é o número atômico do Hidrogênio?',
+    'O o número atômico do Hidrogênio é 1.'
 )
 
 criaCartao(
@@ -14,4 +14,46 @@ criaCartao(
     'Química',
     'Qual é o número atômico do Boro',
     'O número atômico do Boro é 5.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Fósforo?',
+    'O número atômico do Fósforo é 15.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Oxigênio?',
+    'O número atômico do Oxigênio é 8.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Arsênio?',
+    'O número atômico do Arsênio é 33.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Cloro?',
+    'O número atômico do Cloro é 17.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Zinco?',
+    'O número atômico do Zinco é 15.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do Cobre?',
+    'O número atômico do Cobre é 29.'
+)
+
+criaCartao(
+    'Química, tabela periódica',
+    'O que são números atômicos?',
+    'O número atômico é o número que representa a quantidade de prótons no núcleo de um atômo, e os números atômicos são a base da organização da tabela periódica.'
 )
